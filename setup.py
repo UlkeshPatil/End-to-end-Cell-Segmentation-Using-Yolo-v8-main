98580bd663b729653e52dabd87f3c5986aba40e3
UlkeshPatil <ulkeshpatil1@gmail.com>
2023-09-07 09:59:13 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'cellSegmentation',
+    version= '0.0.0',
+    author= 'Ulkesh',
+    author_email= 'ulkesh13@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
